492d0a1beb6157f9711f3a281345d4537088c83c
pooja <bhorpooja222gmail.com>
2017-08-29 14:59:04 +0530

    String Reverse

--- src/JavaString/StringReverse.java
+++ src/JavaString/StringReverse.java
@@ -1,26 +1,27 @@
 package JavaString;
 
+import java.io.IOException;
+
 /**
  * Created by pooja on 29/8/17.
  */
-class Demo{
-    String str="Pooja";
-    int i;
-    char[] arr=str.toCharArray();
-    void reverseString()
-    {
-        for ( i=arr.length-1;i>=0;i--) {
-
-            System.out.print(arr[i] + "\n");
-
-
-
-        }//System.out.println(String.valueOf(arr1));
-    }
-}
 public class StringReverse {
-    public static void main(String[] args) {
-        Demo d=new Demo();
-        d.reverseString();
-}
+   static String str="pooja";
+   static char[] arr=str.toCharArray();
+   static char[] arr1=new char[arr.length];
+   static  String str1;
+  static int i,j=0;
+
+
+    public static void main(String[] args) throws IOException {
+        for(i=arr.length-1;i>=0;i--)
+        {
+            arr1[j]=arr[i];
+            j++;
+        }
+
+        str1=String.valueOf(arr1);
+        System.out.println(str1);
+
+    }
 }
